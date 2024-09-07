ae2bdedb5dfb26a3173376b38fa7bb64cd6c5342
Edvomberto Honorato <e.honorato@uol.com.br>
2024-09-07 13:59:08 -0300

    .......

--- src/components/ProductModal.js
+++ src/components/ProductModal.js
@@ -15,15 +15,19 @@ import {
 
 const ProductModal = ({ item, isOpen, onClose, cds }) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="xl">
-      <ModalOverlay />
-      <ModalContent bg="white"> {/* Cor de fundo do modal (branco) */}
+    <Modal isOpen={isOpen} onClose={onClose} size="lg" scrollBehavior="inside">
+      {/* Modal Overlay com leve opacidade */}
+      <ModalOverlay bg="blackAlpha.600" />
+      
+      {/* Modal Content com sombra e bordas arredondadas */}
+      <ModalContent bg="white" boxShadow="lg" borderRadius="md">
         <ModalHeader>Detalhes do Produto</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
-          {/* Detalhes do Produto */}
-          <Box mb={4} p={4} borderWidth="1px" borderRadius="lg">
-            <h3>Dados do Produto</h3>
+
+        {/* ModalBody com rolagem e espaçamento ajustado */}
+        <ModalBody maxHeight="400px" overflowY="auto" p={6}>
+          <Box mb={4} borderWidth="1px" borderRadius="md" p={4}>
+            <h3 style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>Dados do Produto</h3>
             <p><strong>Produto:</strong> {item.itemDescricao}</p>
             <p><strong>ERP:</strong> {item.erp}</p>
             <p><strong>Destino:</strong> {item.ufDestino}</p>
@@ -32,11 +36,11 @@ const ProductModal = ({ item, isOpen, onClose, cds }) => {
 
           {/* Detalhes da Distribuição */}
           <Box>
-            <h3>Detalhes da Distribuição</h3>
-            <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+            <h3 style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>Detalhes da Distribuição</h3>
+            <Grid templateColumns="repeat(3, 1fr)" gap={4}>
               {cds.map((cd, index) => (
                 <GridItem key={index}>
-                  <Box borderWidth="1px" borderRadius="lg" p={3}>
+                  <Box borderWidth="1px" borderRadius="lg" p={3} boxShadow="sm">
                     <p><strong>{cd.label}</strong></p>
                     {cd.products.map(product => (
                       product.productId === item.erp && (
